58bdede5a77d54c989b5a0b7df91852daa9ed4b0
Alan Santos <alansantos04@hotmail.com>
2022-02-11 00:19:39 -0300

    Atualização de diretórios e reset do email e senha
    Fiz o reset do email e senha, pois quem quiser usar, basta apenas colocar o seu email e senha cadastrado do gmail para que funcione, tornando assim um template para quem quiser verificar ou utilizar.

---
+++ App-Send-Email/processa-envio.php
@@ -0,0 +1,12 @@
+<?php
+
+require "../Private/lib/src/Exception.php";
+require "../Private/lib/src/OAuth.php";
+require "../Private/lib/src/PHPMailer.php";
+require "../Private/lib/src/POP3.php";
+require "../Private/lib/src/SMTP.php";
+
+require "../Private/processa-envio.php";
+
+
+?>
---
+++ Private/processa-envio.php
@@ -0,0 +1,143 @@
+<?php
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+    class Mensagem{
+
+    //variaveis
+    private $para = null;
+    private $assunto = null;
+    private $mensagem = null;
+    public $status = array('codigo_status' => null, 'descricao_status' => '');
+
+
+    //funções 'magicas' set e get
+    function __get($name)
+    {
+        return $this->$name;
+    }
+
+    function __set($name, $value)
+    {
+        $this->$name = $value;
+    }
+
+    function mensagemValida()
+    {
+        if(empty($this->para) || empty($this->assunto) || empty($this->mensagem))
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
+    $mensagem = new Mensagem();
+    $mensagem->__set('para', $_POST['para']);
+    $mensagem->__set('assunto', $_POST['assunto']);
+    $mensagem->__set('mensagem', $_POST['mensagem']);
+
+    if(!$mensagem->mensagemValida())
+    {
+        echo 'Mensagem Valida';
+        header('Location: index.php');
+    }
+    
+
+    $mail = new PHPMailer(true);
+
+    try {
+    
+    $mail->SMTPDebug = false;//SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'your@email.com';                     //SMTP username
+    $mail->Password   = 'password';                               //SMTP password
+    $mail->SMTPSecure = 'tls';//PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('youremail@email.com', 'Name');
+    $mail->addAddress($mensagem->__get('para'));     //Add a recipient
+    //$mail->addReplyTo('info@example.com', 'Information');
+    //$mail->addCC('cc@example.com');
+    //$mail->addBCC('bcc@example.com');
+
+    //Attachments
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = $mensagem->__get('assunto');
+    $mail->Body    = $mensagem->__get('mensagem');
+    $mail->AltBody = 'É necessário um client com suporte HTML para ter acesso total ao conteúdo dessa mensagem';
+
+    $mail->send();
+
+    $mensagem->status['codigo_status'] = 1;
+    $mensagem->status['descricao_status'] = 'E-mail enviado com sucesso';
+    
+} catch (Exception $e) {
+
+    $mensagem->status['codigo_status'] = 2;
+    $mensagem->status['descricao_status'] = "Não foi possivel enviar o email. Detalhes do erro: {$mail->ErrorInfo}";
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>App Mail Send</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+</head>
+<body>
+    
+    <div class="container">
+            <div class="py-3 text-center">
+				<img class="d-block mx-auto mb-2" src="img/logo.png" alt="" width="72" height="72">
+				<h2>Send Mail</h2>
+				<p class="lead">Seu app de envio de e-mails particular!</p>
+			</div>
+            <div class="row">
+                <div class="col-md-12">
+
+                    <?php if($mensagem->status['codigo_status'] == 1){ ?>        
+                        
+                        <div class="container">
+                            <h1 class="display-4 text-success">Sucesso</h1>
+                            <p> <?= $mensagem->status['descricao_status'] ?> </p>
+                            <a href="index.php" class="btn btn-success btn-lg mt-5 text-white">Voltar</a>
+                        </div>
+
+                    <?php } ?>    
+
+                    <?php if($mensagem->status['codigo_status'] == 2){ ?>        
+                        
+                        <div class="container">
+                            <h1 class="display-4 text-danger">Ops!</h1>
+                            <p> <?= $mensagem->status['descricao_status'] ?> </p>
+                            <a href="index.php" class="btn btn-success btn-lg mt-5 text-white">Voltar</a>
+                        </div>
+
+                    <?php } ?> 
+
+                     
+
+                </div>
+            </div>
+    </div>
+
+</body>
+</html>
